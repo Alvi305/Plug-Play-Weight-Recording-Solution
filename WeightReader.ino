f8a8f3bac333b4991fc4b35952a9665be144176b
Alvi305 <78257600+Alvi305@users.noreply.github.com>
2023-05-31 17:14:59 +0800

    Created Functions for reading and printing  weight values

--- WeightReader.ino
+++ WeightReader.ino
@@ -1,58 +1,52 @@
 #include <HardwareSerial.h>
 
 // Set UART Pins
-
 #define RX_PIN 16
 #define TX_PIN 17
 
+// Buffer size
 static constexpr int bufferSize = 12;
-
 unsigned char readBuffer[bufferSize];
 
 // HardwareSerial object
-
 HardwareSerial MySerial(2);
 
 void setup() {
-  // put your setup code here, to run once:
+  // Initialize the built-in serial connection for debugging
   Serial.begin(115200);
 
-  MySerial.begin(9600,SERIAL_8N1,RX_PIN,TX_PIN);
+  // Initialize the connection to the Magic Weight Indicator
+  MySerial.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+}
 
+void readDataFromDevice() {
+  if (MySerial.available()) {
+    delay(1);
+    for (int i = 0; i < bufferSize; i++) {
+      readBuffer[i] = MySerial.read();
+    }
+  }
+}
+
+void printBufferValues() {
+  for (int i = 0; i < bufferSize; i++) {
+    Serial.print((char)readBuffer[i]);
+  }
+  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-Serial.println("Read started");
-// make this a function (get data from weight indicator)
-  if (MySerial.available()) {
-  
-   delay(1);
-   for (int i=0; i < bufferSize; i++)
-   {
-    readBuffer[i] = MySerial.read();
-//    readSize += 1;
-   }
-  }
+  Serial.println("Read started");
 
- Serial.println("Read Finished");
+  // Read data from the Magic Weight Indicator
+  readDataFromDevice();
 
- Serial.flush();
+  Serial.println("Read Finished");
+  Serial.flush();
 
-// print the value (make this a function)
+  // Print the buffer values
+  printBufferValues();
 
-  
- for (int i=0; i < bufferSize; i++) 
- {
-  Serial.print((char)readBuffer[i]);
-  
- }
-
-Serial.flush();
-Serial.println();
-
-  
-
-delay(500);
-
+  Serial.flush();
+  delay(500);
 }
